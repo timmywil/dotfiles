cb83247f5a3bd50f85e5195fa423e73fa0146d52
Timmy Willison <timmywillisn@gmail.com>
2014-10-30 17:16:35 -0400

    Add cask installs back to brew.sh

--- brew.sh
+++ brew.sh
@@ -96,3 +96,30 @@ brew install homebrew/versions/lua52
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# Install native apps
+# Usage: `brew bundle Caskfile`
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install dropbox 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+brew cask install google-chrome-canary 2> /dev/null
+brew cask install imagealpha 2> /dev/null
+brew cask install imageoptim 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install macvim 2> /dev/null
+brew cask install miro-video-converter 2> /dev/null
+brew cask install opera 2> /dev/null
+brew cask install opera-developer 2> /dev/null
+brew cask install opera-next 2> /dev/null
+brew cask install the-unarchiver 2> /dev/null
+brew cask install torbrowser 2> /dev/null
+brew cask install transmission 2> /dev/null
+brew cask install ukelele 2> /dev/null
+brew cask install virtualbox 2> /dev/null
+brew cask install vlc 2> /dev/null
+
+# Remove outdated versions of apps
+brew cask cleanup
