8da4f678a08f08fe3591a711435db4aa4016ca09
Timmy Willison <4timmywil@gmail.com>
2016-09-10 13:39:01 -0400

    update bootstrap and gpg for windows

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")";
 
-rsync --exclude ".git/" \
-	--exclude ".DS_Store" \
-	--exclude ".idea/" \
-	--exclude ".osx" \
-	--exclude ".gitignore" \
-	--exclude "bootstrap.sh" \
-	--exclude "README.md" \
-	--exclude "LICENSE-MIT.txt" \
-	--exclude "emacs.d/" \
-	-avh --no-perms . ~;
+cp ./.aliases ~/
+cp ./.bash_profile ~/
+cp ./.bash_prompt ~/
+cp ./.extra ~/
+cp ./.gitconfig ~/
+cp ./.gitignore-global ~/
+cp ./.functions ~/
+cp ./.exports ~/
+#cp ./.gnupg/gpg-agent.conf ~/.gnupg/
+cp ./.gnupg/gpg.conf ~/.gnupg/
+#cp -r ~/.gnupg/* ~/AppData/Roaming/gnupg/
 
-rsync --exclude ".git/" \
-      --exclude ".gitignore" \
-      --exclude "elpa/" \
-      -avh --no-perms emacs.d/ ~/.emacs.d/
 source ~/.bash_profile;
