da66495226fbced9d9fd045c070feea662e1185e
Timmy Willison <4timmywil@gmail.com>
2016-10-29 12:30:17 -0400

    brew.sh: lein -> leiningen

--- brew.sh
+++ brew.sh
@@ -104,7 +104,7 @@ brew install tree
 brew install vbindiff
 brew install webkit2png
 brew install zopfli
-brew install lein
+brew install leiningen
 
 # Install docker
 brew install docker docker-machine
