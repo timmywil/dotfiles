eda8e4eb50a7e7417d338104f14693f70c13e9f8
Timmy Willison <4timmywil@gmail.com>
2016-11-08 14:41:20 -0500

    [brew.sh] install java sdk with brew

--- brew.sh
+++ brew.sh
@@ -145,6 +145,7 @@ brew cask install google-chrome-canary 2> /dev/null
 brew cask install imagealpha 2> /dev/null
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
+brew cask install java 2> /dev/null
 brew cask install macvim 2> /dev/null
 brew cask install opera 2> /dev/null
 brew cask install opera-developer 2> /dev/null
